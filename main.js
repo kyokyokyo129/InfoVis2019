7b01951028e8ddac5531e382a5bb1037295621bd
kyokyokyo129 <49426795+kyokyokyo129@users.noreply.github.com>
2019-04-17 14:13:41 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -22,6 +22,10 @@ function main()
     var cube = new THREE.Mesh( geometry, material );
     scene.add( cube );
 
+    var light = new THREE.PointLight( 0xffffff );
+    light.position.set( 1, 1, 1 );
+    scene.add( light );
+
     loop();
 
     function loop()
